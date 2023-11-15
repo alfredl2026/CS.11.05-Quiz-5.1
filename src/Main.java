698365a30ea658e8104168d1a94093338eb36a3b
Alfred Lam <pakto1230@gmail.com>
2023-11-15 08:21:43 +0800

    Removed redundant comments.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
+import java.util.Arrays;
 
+public class Main {
+public static void main(String[] args){
+    int[] array1 = new int[] {1,2,3,4,5};
+    int[] array2 = new int[] {6,7,8,9,10};
+    combine(array1, array2);
+    zip(array1, array2);
+}
     /**
      * Given two arrays of integers, write a method called combine that combines the two arrays into one larger array.
      * The method combine will return a new one dimensional array of integers.
@@ -8,8 +15,15 @@ public class Main {
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
-
-        return null;
+    int[] array3 = new int[array1.length + array2.length];
+    for (int i = 0; i<array1.length; i++){
+        array3[i]=array1[i];
+    }
+    for (int i = 0; i<array2.length; i++){
+        array3[array1.length+i]=array2[i];
+    }
+    System.out.println(Arrays.toString(array3));
+        return array3;
 
     }
 
@@ -21,9 +35,19 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] array3 = new int[array1.length + array2.length];
+        int j = 0;
+        for (int i = 0; i<array3.length; i+=2){
+            array3[i]=array1[j];
+            j++;
+        }
+        j=0;
+        for (int i = 1; i<array3.length; i+=2){
+            array3[i]=array2[j];
+            j++;
+        }
+        System.out.println(Arrays.toString(array3));
+        return array3;
     }
 
     /**
@@ -34,8 +58,11 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
-
-        return null;
+    int[] array3 = new int[array1.length];
+    for (int i = 0; i<array3.length; i++){
+        array3[i]=array1[i]*array2[i];
+    }
+        return array3;
 
     }
 
@@ -56,14 +83,21 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
-
-        return null;
+        int[] array1 = new int[words.length];
+        for(int i = 0; i<words.length; i++){
+           array1[i] = countCapitalLetters(words[i]);
+        }
+        return array1;
 
     }
 
     public static int countCapitalLetters(String word) {
-
-        return 0;
+        int count = 0;
+        for(int i = 0; i<word.length(); i++){
+            int num = (int) word.charAt(i);
+            if(num>=65 && num<=90) count++;
+        }
+        return count;
 
     }
 
